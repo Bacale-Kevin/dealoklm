5b0a2b5eb80e7c00835853b570364126a1183f0e
Bacale Kevin <bacale86@gmail.com>
2021-06-04 18:54:52 +0100

    bon-plan-bank

--- components/bon-plan-bank/dummyData.js
+++ components/bon-plan-bank/dummyData.js
@@ -93,7 +93,7 @@ export const dummyDataDetail = [
       "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
   },
   {
-    id: 1,
+    id: 2,
     imageUrl: "/images/boursorama_banque_carre.png",
     title: "Afriland First Bank",
     bonPlanTitle: "Afriland First Bank",
@@ -136,9 +136,9 @@ export const dummyDataDetail = [
       "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
   },
   {
-    id: 1,
+    id: 3,
     imageUrl: "/images/boursorama_banque_carre.png",
-    title: "Afriland First Bank",
+    title: "Central bank",
     bonPlanTitle: "Central bank",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     step1: "Step 1",
