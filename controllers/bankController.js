9cc0ecffed5407e2b9412590ded939b334d7e937
Bacale Kevin <bacale86@gmail.com>
2021-06-05 09:14:57 +0100

    bank model

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -1,10 +1,27 @@
-const getAllBanks = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'All banks'
-    })
-}
+import Bank from "../model/bank";
 
-export {
-    getAllBanks,
-}
+const getAllBanks = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "All banks",
+  });
+};
+
+//* Create new room => /api/bank
+const createBank = async (req, res) => {
+  try {
+    const bank = await Bank.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      bank,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
+export { getAllBanks, createBank };
--- pages/api/banks/index.js
+++ pages/api/banks/index.js
@@ -1,11 +1,12 @@
 import dbConnect from 'config/dbConnect'
 import nc from 'next-connect'
-import { getAllBanks } from "../../../controllers/bankController"
+import { createBank, getAllBanks } from "../../../controllers/bankController"
 
 const handler = nc()
 
 dbConnect()
 
 handler.get(getAllBanks)
+handler.post(createBank)
 
 export default handler
